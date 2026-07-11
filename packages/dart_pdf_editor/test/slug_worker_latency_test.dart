204e46223e55a01a2e4c875412c549a43e7b8201
dart-pdf release automation probe <actions@github.com>
2026-07-11 18:30:32 +1000

    test: remove unused Slug probe import

--- packages/dart_pdf_editor/test/slug_worker_latency_test.dart
+++ packages/dart_pdf_editor/test/slug_worker_latency_test.dart
@@ -14,7 +14,6 @@ import 'package:dart_pdf_editor/dart_pdf_editor.dart';
 import 'package:dart_pdf_editor/strips.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pdf_document/pdf_document.dart';
-import 'package:pdf_graphics/pdf_graphics.dart';
 
 void main() {
   final path =
